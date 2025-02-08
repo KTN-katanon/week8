8303d762ee1b039f49b018b814fce5aea4bd1321
Katanon Krengsomrong <66160196@go.buu.ac.th>
2025-02-09 03:08:12 +0700

    Add interceptor request and response

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,10 +1,10 @@
-import { defineBoot } from '#q-app/wrappers';
-import axios, { type AxiosInstance } from 'axios';
+import { defineBoot } from '#q-app/wrappers'
+import axios, { type AxiosInstance } from 'axios'
 
 declare module 'vue' {
   interface ComponentCustomProperties {
-    $axios: AxiosInstance;
-    $api: AxiosInstance;
+    $axios: AxiosInstance
+    $api: AxiosInstance
   }
 }
 
@@ -14,18 +14,29 @@ declare module 'vue' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://localhost:3000' });
-
+const api = axios.create({ baseURL: 'http://localhost:3000' })
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+api.interceptors.request.use((config) => {
+  console.log('Request was sent')
+  return config
+})
+api.interceptors.response.use(async (response) => {
+  await sleep(2000)
+  console.log('Response was received')
+  return response
+})
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
 
-  app.config.globalProperties.$axios = axios;
+  app.config.globalProperties.$axios = axios
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
 
-  app.config.globalProperties.$api = api;
+  app.config.globalProperties.$api = api
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
-});
+})
 
-export { api };
+export { api }
